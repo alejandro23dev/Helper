7ec62f39550a4334c83241264413a705a54c27bd
alejandro23dev <alejandro23dev@gmail.com>
2023-06-09 03:09:27 +0100

    update

--- app/Views/header/header.php
+++ app/Views/header/header.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>AlejandroDev | Portafolio</title>
+    <title>Helper</title>
     <!-- LINK BOOSTRAP Y DATATABLE CSS-->
         <link rel="stylesheet" href="<?php echo base_url('BootsTrap/css/bootstrap.min.css');?>">
         <link href="<?php echo base_url('DataTable/DataTables/datatables.min.css');?>" rel="stylesheet"/>
